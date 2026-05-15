b09b6ebcae1fdaaf7c99c75cafb704eb7f6e52f0
Yaseer Shaik <yaseer0328@gmail.com>
2026-05-15 15:03:15 +0530

    Update restaurant.js

--- data/restaurant.js
+++ data/restaurant.js
@@ -30,19 +30,19 @@ export const restaurant = {
     {
       name: "Chicken Manchurian",
       description: "Spicy, crispy, and packed with flavor.",
-      image: "/images/dish1.jpg",
+      image: "/images/dish-1.webp",
     },
 
     {
       name: "Bamboo Chicken",
       description: "Authentic smoky grilled taste.",
-      image: "/images/dish2.jpg",
+      image: "/images/dish-2.webp",
     },
 
     {
       name: "Grilled Chicken",
       description: "Perfectly grilled and fire roasted.",
-      image: "/images/dish3.jpg",
+      image: "/images/dish-3.webp",
     },
   ],
 };
