eaed5f2865f2150a2253a76e2e744cf698c2310e
Yaseer Shaik <yaseer0328@gmail.com>
2026-05-16 15:46:49 +0530

    Update MobileCTA.jsx

--- components/MobileCTA.jsx
+++ components/MobileCTA.jsx
@@ -3,7 +3,7 @@ export default function MobileCTA() {
     <div className="fixed bottom-0 left-0 w-full bg-black border-t border-white/10 z-50 md:hidden">
       <div className="grid grid-cols-3">
         <a
-          href="tel:+919100010203"
+          href="tel:+911234567890"
           className="py-4 text-center font-semibold border-r border-white/10"
         >
           Call
@@ -18,7 +18,7 @@ export default function MobileCTA() {
         </a>
 
         <a
-          href="https://wa.me/919100010203"
+          href="https://wa.me/911234567890"
           target="_blank"
           className="py-4 text-center font-semibold text-[#ff4d6d]"
         >
